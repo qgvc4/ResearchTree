5d3117eaa160ad2d8f7b6f59a86f432413d883a5
Morgan Knoch <morgankn11@gmail.com>
2018-09-30 16:48:57 -0500

    Integration of Entity Framework Core. Created DbContext and Initial DB migration.

---
+++ ResearchTree/Models/ResearchTreeContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResearchTree.Models
+{
+    public class ResearchTreeContext : DbContext
+    {
+        public ResearchTreeContext(DbContextOptions<ResearchTreeContext> options)
+            : base(options)
+        {
+
+        }
+
+        
+    }
+}
