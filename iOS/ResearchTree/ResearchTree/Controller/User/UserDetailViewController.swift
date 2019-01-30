988f59cff26ff8da1f1b55082c8edf53cd65b21a
qgvc4 <qgvc4@mail.missouri.edu>
2019-01-30 17:17:45 -0600

    user detail page

---
+++ iOS/ResearchTree/ResearchTree/Controller/User/UserDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserDetailViewController.swift
+//  ResearchTree
+//
+//  Created by Qiwen Guo on 1/30/19.
+//  Copyright © 2019 Qiwen Guo. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS/ResearchTree/ResearchTree/Controller/User/UserPostOrJobViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserPostOrJobViewController.swift
+//  ResearchTree
+//
+//  Created by Qiwen Guo on 1/30/19.
+//  Copyright © 2019 Qiwen Guo. All rights reserved.
+//
+
+import UIKit
+
+class UserPostOrJobViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
