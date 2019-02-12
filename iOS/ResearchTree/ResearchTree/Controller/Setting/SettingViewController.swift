ec802205018c35f106c2c8e595e2424e69d84837
qgvc4 <qgvc4@mail.missouri.edu>
2019-02-12 15:32:47 -0600

    add setting view

--- iOS/ResearchTree/ResearchTree/Controller/Setting/SettingViewController.swift
+++ iOS/ResearchTree/ResearchTree/Controller/Setting/SettingViewController.swift
@@ -10,27 +10,62 @@ import UIKit
 
 class SettingViewController: UIViewController {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var userDescription: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var userLocation: UILabel!
+    @IBOutlet weak var userMajors: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let isUserLoggedIn = UserDefaults.standard.bool(forKey: "isUserLoggedIn")
+        
+        if (isUserLoggedIn) {
+            let userData = UserDefaults.standard.data(forKey: "userData")
+            let decoder = JSONDecoder()
+            do {
+                let user = try decoder.decode(User.self, from: userData!)
+                username.text = "\(user.firstname) \(user.lastname)"
+                userEmail.text = "\(user.email)"
+                userDescription.text = "\(user.description ?? "")"
+                userLocation.text = "\(user.location ?? "")"
+                profileImageView.image = base64ToImage(base64: user.image)
+                
+                var majorString = ""
+                for major in user.majors {
+                    majorString += MajorMap.getString(major: Major(rawValue: major)!)
+                    majorString += "\n"
+                }
+                userMajors.text = majorString
+                
+            } catch {
+                print("decode error")
+                return
+            }
+        }
     }
     
 
+    
     @IBAction func logoutTapped(_ sender: Any) {
         UserDefaults.standard.set(nil, forKey: "userData")
         UserDefaults.standard.set(false, forKey:"isUserLoggedIn")
         let loginViewController = storyboard?.instantiateViewController(withIdentifier: "login") as! LoginViewController
         self.present(loginViewController, animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func base64ToImage(base64: String?) -> UIImage {
+        if base64 == nil {
+            return UIImage(named: "DefaultProfile")!
+        }
+        let data = Data(base64Encoded: base64!, options: Data.Base64DecodingOptions.ignoreUnknownCharacters)
+        
+        if let data = data {
+            if let imageTemp = UIImage(data: data) {
+                return imageTemp
+            }
+        }
+        
+        return UIImage(named: "DefaultProfile")!
     }
-    */
-
 }
