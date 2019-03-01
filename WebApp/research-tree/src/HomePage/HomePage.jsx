a48aa3cda3a34eeb0cb1791f97ff2d35000be038
Mason Breece <mrbrhc@mail.missouri.edu>
2019-03-01 11:49:31 -0600

    navbar adjustment

--- WebApp/research-tree/src/HomePage/HomePage.jsx
+++ WebApp/research-tree/src/HomePage/HomePage.jsx
@@ -6,7 +6,9 @@ import { userActions } from '../_actions';
 
 //antd
 import { Layout, Menu, Icon } from 'antd';
-//import '../../node_modules/antd/dist/antd.css';
+
+//c1e791 green
+
 
 const {
   Header, Content, Footer, Sider,
@@ -20,60 +22,51 @@ class HomePage extends React.Component {
     handleDeleteUser(id) {
         return (e) => this.props.dispatch(userActions.delete(id));
     }
+    
+    
 
     render() {
         const { user, users } = this.props;
         return (
             <Layout>
                 <Sider style={{
-                  overflow: 'auto', height: '100vh', position: 'fixed', left: 0,
+                  overflow: 'auto', height: '100%', position: 'fixed', left: 0,
+                      background: '#c1e791'
                 }}
                 >
                   <div className="logo" />
-                  <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+                    
+                    <h2>ResearchTree</h2>
+                    
+                  <Menu theme="" mode="inline" defaultSelectedKeys={['1']} style={{ background: '#c1e791' }}>
                     <Menu.Item key="1">
                       <Icon type="user" />
-                      <span className="nav-text">nav 1</span>
+                      <span className="nav-text">Feed</span>
                     </Menu.Item>
                     <Menu.Item key="2">
-                      <Icon type="video-camera" />
-                      <span className="nav-text">nav 2</span>
+                      <Icon type="team" />
+                      <span className="nav-text">People</span>
                     </Menu.Item>
                     <Menu.Item key="3">
-                      <Icon type="upload" />
-                      <span className="nav-text">nav 3</span>
+                      <Icon type="shop" />
+                      <span className="nav-text">Jobs</span>
                     </Menu.Item>
                     <Menu.Item key="4">
                       <Icon type="bar-chart" />
-                      <span className="nav-text">nav 4</span>
+                      <span className="nav-text">Settings</span>
                     </Menu.Item>
-                    <Menu.Item key="5">
-                      <Icon type="cloud-o" />
-                      <span className="nav-text">nav 5</span>
-                    </Menu.Item>
-                    <Menu.Item key="6">
-                      <Icon type="appstore-o" />
-                      <span className="nav-text">nav 6</span>
-                    </Menu.Item>
-                    <Menu.Item key="7">
-                      <Icon type="team" />
-                      <span className="nav-text">nav 7</span>
-                    </Menu.Item>
-                    <Menu.Item key="8">
-                      <Icon type="shop" />
-                      <span className="nav-text">nav 8</span>
-                    </Menu.Item>
+                  
                   </Menu>
                 </Sider>
-                <Layout style={{ marginLeft: 200 }}>
-                  <Header style={{ background: '#fff', padding: 0 }} />
-                  <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                <Layout style={{  }}>
+                  
+                  <Content style={{ margin: '',background: '#fff', overflow: 'initial' }}>
                     <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
                       ...
                       <br />
-                      Really
+                      Place
                       <br />...<br />...<br />...<br />
-                      long
+                      Holder
                       <br />...<br />...<br />...<br />...<br />...<br />...
                       <br />...<br />...<br />...<br />...<br />...<br />...
                       <br />...<br />...<br />...<br />...<br />...<br />...
@@ -84,11 +77,10 @@ class HomePage extends React.Component {
                       content
                     </div>
                   </Content>
-                  <Footer style={{ textAlign: 'center' }}>
-                    Ant Design ©2018 Created by Ant UED
-                  </Footer>
+                  
                 </Layout>
               </Layout>
+            
         );
     }
 }
