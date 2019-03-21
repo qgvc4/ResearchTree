3b0f9c004e1023ec5210a101fe301adf504e9c13
Qiwen Guo <qgvc4@mail.missouri.edu>
2019-03-21 16:29:05 -0700

    add attachment for new post

--- WebApp/research-tree/src/components/Feed/PostFeed.jsx
+++ WebApp/research-tree/src/components/Feed/PostFeed.jsx
@@ -8,6 +8,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import {newPost, fetchPosts} from '../../actions/FeedAction'
+import ImageUploader from 'react-images-upload';
 
 import '../../style/Feed/postFeed.css';
 
@@ -17,18 +18,40 @@ class PostFeed extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
-                values["PeopleId"] = `${this.props.user.id}`;
+          if (!err) {
+              console.log('Received values of form: ', values);
+              values["PeopleId"] = `${this.props.user.id}`;
+
+              if (values["attachment"]) {
+                new Promise((resolve, reject) => {
+                  var reader = new FileReader();
+                  reader.addEventListener("load", () => {
+                      var data = reader.result;
+                      var res = data.split(",");
+                      var imageData = res[1];
+                      resolve(imageData);
+                  });
+                  reader.readAsDataURL(values["attachment"]);
+                }).then(imageData => {
+                  values["attachment"] = imageData;
+                  this.props.newPost(this.props.user.token, values);
+                });
+              } else {
                 this.props.newPost(this.props.user.token, values);
-            }
+              }
+          }
         });
     }
+
+    normFile = (e) => {
+      if (Array.isArray(e) && e.length > 0) {
+        return e[0];
+      }
+      return null;
+    }
+  
     
     render() {
-        // if (!this.props.isLoading) {
-        //     this.props.fetchPosts(this.props.user.token)
-        // } 
         const { getFieldDecorator } = this.props.form;
         return (
           <Form onSubmit={this.handleSubmit} className="post-feed" style={{margin: '2%'}}>
@@ -46,6 +69,25 @@ class PostFeed extends Component {
                 <TextArea placeholder="Description" autosize={{ minRows: 2, maxRows: 6 }} />
                 )}
             </Form.Item>
+            <Form.Item
+            label="attachment"
+          >
+            {getFieldDecorator('attachment', {
+              valuePropName: 'fileList',
+              getValueFromEvent: this.normFile,
+            })(
+              <ImageUploader
+                withIcon={true}
+                withLabel={true}
+                label='JPG|Size<1MB'
+                buttonText='Upload attachment'
+                onChange={this.onDrop}
+                imgExtension={['.jpg']}
+                maxFileSize={1048576}
+                withPreview={true}
+            />
+            )}
+          </Form.Item>
             <Form.Item>
               <Button type="primary" htmlType="submit" className="login-form-button">
                 Post
